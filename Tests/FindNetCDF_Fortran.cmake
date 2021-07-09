273798890a7ade072b434f28c6e54f91f5c659ac
Lionel GUEZ <guez@lmd.ens.fr>
2021-07-10 00:10:03 +0200

    Update
    Update to commit cb25161 from cmake repository.

--- Tests/FindNetCDF_Fortran.cmake
+++ Tests/FindNetCDF_Fortran.cmake
@@ -1,29 +1,35 @@
-find_package(PkgConfig REQUIRED)
-pkg_check_modules(NetCDF_Fortran REQUIRED IMPORTED_TARGET netcdf-fortran)
-pkg_get_variable(netcdf_fortran_pcfiledir netcdf-fortran pcfiledir)
-message(STATUS "${CMAKE_CURRENT_LIST_FILE}")
-message(STATUS "-- Location of .pc file: ${netcdf_fortran_pcfiledir}")
+if (NOT TARGET NetCDF_Fortran::NetCDF_Fortran)
+  find_package(PkgConfig REQUIRED)
 
-message(STATUS
-  "-- NetCDF-Fortran include directories: ${NetCDF_Fortran_INCLUDE_DIRS}")
+  pkg_check_modules(NetCDF_Fortran REQUIRED IMPORTED_TARGET GLOBAL
+    netcdf-fortran)
 
-message(STATUS "-- NetCDF-Fortran libraries: ${NetCDF_Fortran_LINK_LIBRARIES}")
-
-if(PKG_CONFIG_VERSION_STRING VERSION_LESS "0.29.2"
-    OR PKG_CONFIG_VERSION_STRING VERSION_GREATER "1.1")
-  # pkg-config strips system flags out of cflags. They do not appear
-  # with prefix -isystem when pkg-config is run with
-  # --cflags-only-I. So cmake does not get them. So we have to
-  # duplicate the call to pkg_get_variable that is already in
-  # pkg_check_modules.
-  # Version > 1.1 means we are using pkgconf from pkgconf.org
-  pkg_get_variable(pkg_netcdf_fortran_includedir netcdf-fortran includedir)
-
-  target_include_directories(PkgConfig::NetCDF_Fortran INTERFACE
-    ${pkg_netcdf_fortran_includedir})
+  pkg_get_variable(netcdf_fortran_pcfiledir netcdf-fortran pcfiledir)
+  message(STATUS "${CMAKE_CURRENT_LIST_FILE}")
+  message(STATUS "-- Location of .pc file: ${netcdf_fortran_pcfiledir}")
 
   message(STATUS
-    "-- pkg_netcdf_fortran_includedir: ${pkg_netcdf_fortran_includedir}")
-endif()
+    "-- NetCDF-Fortran include directories: ${NetCDF_Fortran_INCLUDE_DIRS}")
 
-add_library(NetCDF_Fortran::NetCDF_Fortran ALIAS PkgConfig::NetCDF_Fortran)
+  message(STATUS
+    "-- NetCDF-Fortran libraries: ${NetCDF_Fortran_LINK_LIBRARIES}")
+
+  if(PKG_CONFIG_VERSION_STRING VERSION_LESS "0.29.2"
+      OR PKG_CONFIG_VERSION_STRING VERSION_GREATER "1.1")
+    # pkg-config strips system flags out of cflags. They do not appear
+    # with prefix -isystem when pkg-config is run with
+    # --cflags-only-I. So cmake does not get them. So we have to
+    # duplicate the call to pkg_get_variable that is already in
+    # pkg_check_modules.
+    # Version > 1.1 means we are using pkgconf from pkgconf.org
+    pkg_get_variable(pkg_netcdf_fortran_includedir netcdf-fortran includedir)
+
+    target_include_directories(PkgConfig::NetCDF_Fortran INTERFACE
+      ${pkg_netcdf_fortran_includedir})
+
+    message(STATUS
+      "-- pkg_netcdf_fortran_includedir: ${pkg_netcdf_fortran_includedir}")
+  endif()
+
+  add_library(NetCDF_Fortran::NetCDF_Fortran ALIAS PkgConfig::NetCDF_Fortran)
+endif()
